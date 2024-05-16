3a0789afa6c8128654ffb69574af71daddfca4f1
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-16 11:38:55 -0600

    Testing the function to use the API

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,7 +1,17 @@
 import axios from "axios";
+import { SearchType } from "../types";
 
 export default function useWeather() {
-  const fetchWeather = () => {};
+  const fetchWeather = async (search: SearchType) => {
+    const appID = import.meta.env.VITE_APIKEY;
+    try {
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appID}`;
+
+      const { data } = await axios(geoUrl);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return {
     fetchWeather,
