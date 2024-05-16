cdda831df29761c331614945beae94f01df8ee0d
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-16 10:50:48 -0600

    Styling the form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,16 @@
 import { countries } from "../../data/countries";
+import styles from './Form.module.css'
 
 export default function Form() {
     return (
-        <form>
+        <form className={styles.form}>
 
-            <div>
+            <div className={styles.field}>
                 <label htmlFor="city">Cuidad:</label>
                 <input type="text" name="city" id="city" placeholder="Cuidad" />
             </div>
 
-            <div>
+            <div className={styles.field}>
                 <label htmlFor="city">País:</label>
                 <select name="" id="">
                     <option value="">-- Selecione un País</option>
@@ -25,7 +26,7 @@ export default function Form() {
                 </select>
             </div>
 
-            <input type="submit" value="Consultar Clima" />
+            <input type="submit" value="Consultar Clima" className={styles.submit} />
         </form>
     );
 };
