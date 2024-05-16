17f4bdf6dcd5fe7b2fe713b14ce345179b26220a
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-16 11:00:47 -0600

    Adding the form values in the state

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { countries } from "../../data/countries";
 import styles from './Form.module.css'
 import { SearchType } from "../../types";
@@ -9,17 +9,34 @@ export default function Form() {
         country: '',
     })
 
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
+
     return (
         <form className={styles.form}>
 
             <div className={styles.field}>
                 <label htmlFor="city">Cuidad:</label>
-                <input type="text" name="city" id="city" placeholder="Cuidad" />
+                <input
+                    type="text"
+                    name="city"
+                    id="city"
+                    placeholder="Cuidad"
+                    value={search.city}
+                    onChange={handleChange} />
             </div>
 
             <div className={styles.field}>
-                <label htmlFor="city">País:</label>
-                <select name="" id="">
+                <label htmlFor="country">País:</label>
+                <select
+                    id="country"
+                    name="country"
+                    onChange={handleChange}
+                    value={search.country}>
                     <option value="">-- Selecione un País</option>
                     {
                         countries.map(country => (
