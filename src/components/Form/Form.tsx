a6d33393860447080f16751b84b3699cd8c443e3
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-16 10:36:46 -0600

    Fixing issue woth form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -11,16 +11,18 @@ export default function Form() {
 
             <div>
                 <label htmlFor="city">País:</label>
-                <option value="">-- Selecione un País</option>
-                {
-                    countries.map(country => (
-                        <option
-                            key={country.code}
-                            value={country.code}>
-                            {country.name}
-                        </option>
-                    ))
-                }
+                <select name="" id="">
+                    <option value="">-- Selecione un País</option>
+                    {
+                        countries.map(country => (
+                            <option
+                                key={country.code}
+                                value={country.code}>
+                                {country.name}
+                            </option>
+                        ))
+                    }
+                </select>
             </div>
 
             <input type="submit" value="Consultar Clima" />
