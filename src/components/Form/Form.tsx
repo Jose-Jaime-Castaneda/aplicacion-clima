4e47b00f53c760c059795ae6706c05aabd301c21
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-05-16 10:36:13 -0600

    Renaming components with finget error and showing the form in the app component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,29 @@
+import { countries } from "../../data/countries";
+
+export default function Form() {
+    return (
+        <form>
+
+            <div>
+                <label htmlFor="city">Cuidad:</label>
+                <input type="text" name="city" id="city" placeholder="Cuidad" />
+            </div>
+
+            <div>
+                <label htmlFor="city">País:</label>
+                <option value="">-- Selecione un País</option>
+                {
+                    countries.map(country => (
+                        <option
+                            key={country.code}
+                            value={country.code}>
+                            {country.name}
+                        </option>
+                    ))
+                }
+            </div>
+
+            <input type="submit" value="Consultar Clima" />
+        </form>
+    );
+};
